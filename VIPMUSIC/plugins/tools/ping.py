698bf4b409d90f4bedaf8004c0ce49a1c3712e07
Khushijha5544 <133341324+Khushijha5544@users.noreply.github.com>
2024-07-17 17:06:49 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -111,11 +111,11 @@ async def ping_com(client, message: Message, _):
                 [
                     InlineKeyboardButton(
                         text="✦ ɢʀᴏᴜᴘ ✦",
-                        url=f"https://t.me/TG_FRIENDSS",
+                        url=f"https://t.me/MEETUP_ZONE",
                     ),
                     InlineKeyboardButton(
                         text="✧ ᴍᴏʀᴇ ✧",
-                        url=f"https://t.me/VIP_CREATORS",
+                        url=f"https://t.me/AB_KRISHNA_UFF",
                     ),
                 ],
                 [
