3abcdea0e0b485709224a4a5eeea4ba5194344f2
Khushijha5544 <133341324+Khushijha5544@users.noreply.github.com>
2024-07-17 17:10:08 +0530

    Update shayri.py

--- VIPMUSIC/plugins/USERBOT/shayri.py
+++ VIPMUSIC/plugins/USERBOT/shayri.py
@@ -84,10 +84,10 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/TG_FRIENDSS"
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/MEETUP_ZONE"
                     ),
                     InlineKeyboardButton(
-                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/VIP_CREATORS"
+                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/AB_KRISHNA_UFF"
                     ),
                 ]
             ]
